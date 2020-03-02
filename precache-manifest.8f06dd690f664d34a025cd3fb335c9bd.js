e41e531f31548a6ba06d3cfb1ec1a0f001b938fd
Mr. Rabbit <oldwhite37201@users.noreply.github.com>
2020-03-02 10:28:02 +0700

    Updates

---
+++ precache-manifest.8f06dd690f664d34a025cd3fb335c9bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa2414ec99145a2b4ce6d274b04e996",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "16ad1effc920e083dffb",
+    "url": "/pomodoro/static/css/main.448cdb03.chunk.css"
+  },
+  {
+    "revision": "244a5f74f36f16848e8b",
+    "url": "/pomodoro/static/js/2.1a8ae964.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/pomodoro/static/js/2.1a8ae964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ad1effc920e083dffb",
+    "url": "/pomodoro/static/js/main.daef2691.chunk.js"
+  },
+  {
+    "revision": "73f0e59f39054b865559",
+    "url": "/pomodoro/static/js/runtime-main.0f092aee.js"
+  }
+]);
